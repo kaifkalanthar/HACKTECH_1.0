1d3a742227ace9638e5999fc8dccc1c2e63aa358
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-03-29 12:40:54 +0530

    ProblemS

--- fronend/src/App.js
+++ fronend/src/App.js
@@ -1,11 +1,15 @@
+import AboutPage from "./component/aboutPage";
 import HomePage from "./component/homePage";
 import NavBar from "./component/navBar";
+import ProbStat from "./component/problemStatement";
 
 function App() {
   return (
     <>
       <NavBar />
       <HomePage />
+      <AboutPage />
+      <ProbStat />
     </>
   );
 }
