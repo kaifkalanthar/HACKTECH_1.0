a6e3da42bbeb02d66851dfeb5b6c5d76a6b79f11
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-03-30 14:30:07 +0530

    version-completed

--- fronend/src/component/sponsers.jsx
+++ fronend/src/component/sponsers.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import '../style/sponsers.css';
+import google from '../externals/google.png';
+import ibm from '../externals/ibm.png';
+import acc from '../externals/acc.png';
 
 const Sponsers = () => {
     return (
@@ -6,9 +10,9 @@ const Sponsers = () => {
             <div className="sponsers-container">
                 <h1 className="sponsers-header">OUR SPONSERS</h1>
                 <div className="sponsers-list">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <img src={google} alt="" className='sponsers-img'/>
+                    <img src={ibm} alt=""  className='sponsers-img'/>
+                    <img src={acc} alt="" className='sponsers-img'/>
                 </div>
             </div>
         </div>
